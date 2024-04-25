dc82b1b7510ca60add32719e996bae7025b78b18
Researcher <70377003+abdulmoiz-bit@users.noreply.github.com>
2024-04-25 17:51:35 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='toast'>This app is in development process</div>
+    </>
+  )
+}
+
+export default App
